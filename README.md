ae7a581b5fda13d63aff258dede4e7f24e9242e9
Harry <hariharasethi89@gmail.com>
2025-09-10 19:56:42 +0530

    Project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# Yt2Mp3
+This projects converts the _youtube_ videos to _mp3_ format
---
+++ client/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-4xl text-blue-400">what the dog doing!</div>;
+}
+
+export default App;
